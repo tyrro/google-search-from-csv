318e7d5b87167033256bb84043c6b10dce8ffb5f
rajibds <rajib.das.shuva@gmail.com>
2022-01-08 14:45:43 +0600

    feat(backend): Implement keywords#index to test out clearance

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_045254) do
+ActiveRecord::Schema.define(version: 2022_01_07_130454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "keywords", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "search_status", default: false, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_keywords_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_01_07_045254) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "keywords", "users"
 end
