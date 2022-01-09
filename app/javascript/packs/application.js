cdd12619bb862dab1a282eced50a3f3cfcd79f84
rajibds <rajib.das.shuva@gmail.com>
2022-01-10 01:52:07 +0600

    chore(frontend): Allow webpack to load react components in applicaton.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from '@rails/ujs';
-import Turbolinks from 'turbolinks';
-import * as ActiveStorage from '@rails/activestorage';
-import 'channels';
+import WebpackerReact from 'webpacker-react';
+import 'bootstrap';
+import '../shared/i18n-js';
 
 import '../stylesheets/index.scss';
 
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+import SearchableKeywordTable from '../components/Keywords';
+
+WebpackerReact.setup({ SearchableKeywordTable });
