6478068dae20a2cde21ca49cf4dfebd32d93a94b
rajibds <rajib.das.shuva@gmail.com>
2022-01-10 01:45:02 +0600

    chore(backend): Add default values to keywords columns

--- db/migrate/20220107130454_create_keywords.rb
+++ db/migrate/20220107130454_create_keywords.rb
@@ -3,9 +3,9 @@ class CreateKeywords < ActiveRecord::Migration[6.1]
     create_table :keywords do |t|
       t.string :name, null: false
       t.integer :search_status, default: 0, null: false
-      t.integer :total_adwords
-      t.integer :total_search_results
-      t.float :total_search_time
+      t.integer :total_search_results, default: 0, null: false
+      t.float :total_search_time, default: 0, null: false
+      t.integer :total_adwords, default: 0, null: false
       t.text :search_result_html
       t.references :user, foreign_key: true, null: false
 
