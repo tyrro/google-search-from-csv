971222d9aa8df7db6f5a1c70a19bc5a8895e7610
rajibds <rajib.das.shuva@gmail.com>
2022-01-11 23:46:53 +0600

    chore(backend): Add keyword#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'keywords#index'
 
-  resources :keywords, only: %i(index destroy) do
+  resources :keywords, only: %i(index show) do
     collection do
       post :import
     end
