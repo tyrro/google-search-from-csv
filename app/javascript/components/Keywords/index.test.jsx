44f9137627acbab41ea680eff5613bf66ef742ae
rajibds <rajib.das.shuva@gmail.com>
2022-01-12 12:28:52 +0600

    chore(spec): Add keywords#index test

--- app/javascript/components/Keywords/index.test.jsx
+++ app/javascript/components/Keywords/index.test.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import CarIndex from './index';
+import KeywordIndex from './index';
 
-describe(CarIndex, () => {
-  const wrapper = shallow(<CarIndex />);
+describe(KeywordIndex, () => {
+  const props = { sampleCSVFile: '/link/to/file' };
 
-  it('sets the search param when typed', () => {
-    expect(wrapper.find('input[type="text"]').prop('value')).toEqual('');
-    wrapper.find('input[type="text"]').simulate('change', {
-      target: { value: 'bmw' },
-    });
+  const wrapper = shallow(<KeywordIndex {...props} />);
 
-    expect(wrapper.find('input[type="text"]').prop('value')).toEqual('bmw');
+  test('it renders SearchBar component', async () => {
+    expect(wrapper.find('SearchBar')).toHaveLength(1);
+  });
+
+  test('it renders pagination in two places', async () => {
+    expect(wrapper.find('Pagination')).toHaveLength(2);
   });
 });
