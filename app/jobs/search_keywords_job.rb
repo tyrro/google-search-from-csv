c194d711213238798be5e21b808abba37dffa78f
rajibds <rajib.das.shuva@gmail.com>
2022-01-12 12:27:32 +0600

    chore(spec): Add spec/jobs

--- app/jobs/search_keywords_job.rb
+++
@@ -1,9 +0,0 @@
-class SearchKeywordsJob < ApplicationJob
-  queue_as :default
-
-  def perform(keyword_ids)
-    keyword_ids.each_with_index do |keyword_id, index|
-      SearchKeywordJob.set(wait: index * 2).perform_later(keyword_id)
-    end
-  end
-end
